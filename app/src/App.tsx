94f386e1d35d657d562084bc047380bcd6b2be98
Vanni Matteo <vannicraft@gmail.com>
2026-03-09 21:33:36 +0100

    ok ricreato il progetto in typescript

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -22,15 +22,12 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <p>PIPPO</p>
-      <p>PIPPO</p>
-      <p>PIPPO</p>
     </>
   )
 }
